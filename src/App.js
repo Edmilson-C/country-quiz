03e618c5256978b96ec634e9c38c674f508ddb87
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-01 02:04:38 +0200

    Finish project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import CardBox from './components/card-box/card-box.component'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Country Quiz </h1>
+      <CardBox />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
