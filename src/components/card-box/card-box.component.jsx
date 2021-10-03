cd77fc1a795005ba525feb12b2fb93782ebc0925
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-03 15:05:02 +0200

    feat: add button on card box

--- src/components/card-box/card-box.component.jsx
+++ src/components/card-box/card-box.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import Button from '../button/button.component'
+
 import './card-box.styles.scss'
 
 const CardBox = () => (
   <div className="card-box">
     <h3 className="card-box__heading"> Title will be here </h3>
+    <Button type="answer" option="A" content="Mozambique" />
+    <Button type="answer" option="B" content="Mozambique" />
+    <Button type="answer" option="C" content="Mozambique" />
+    <Button type="answer" option="D" content="Mozambique" />
   </div>
 )
 
