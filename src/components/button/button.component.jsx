d8a1385b697f4b324b4643df0caf2644f2d2727a
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-03 15:10:45 +0200

    style: add color on button hover state

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -5,9 +5,7 @@ import './button.styles.scss'
 const Button = ({ content, option, type }) => (
   <button type="submit" className={`btn btn--${type}`}>
     {option && <span className="btn--answer__option">{option}</span>}
-    <span className="btn--answer__option">A</span>
     {content}
-    Mozambique
   </button>
 )
 
