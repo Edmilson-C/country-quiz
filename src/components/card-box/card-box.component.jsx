7b973e84322d5650582531aff3de8a0c4cd9acda
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-03 14:10:18 +0200

    feat: create card box component

---
+++ src/components/card-box/card-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './card-box.styles.scss'
+
+const CardBox = () => (
+  <div className="card-box">
+    <h3 className="card-box__heading"> Title will be here </h3>
+  </div>
+)
+
+export default CardBox
