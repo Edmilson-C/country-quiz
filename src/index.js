f2f36111fa3e38a92e9f04f2b6f72f343697dbf0
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-04 21:41:44 +0200

    feat: add question provider in index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
+import { QuestionsProvider } from './contexts/questions/questions.context'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QuestionsProvider>
+      <App />
+    </QuestionsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
