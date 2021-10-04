9dba281e94a24bc6f0449e2064eacb6238d74097
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-04 15:48:27 +0200

    refactor: make api request async

--- src/contexts/questions/question.data.js
+++ src/contexts/questions/question.data.js
@@ -1,50 +1,54 @@
 const url = 'https://restcountries.com/v3.1/all?fields=name,capital,flag'
 const requestedData = []
 
-fetch(url)
-  .then((res) => res.json())
-  .then((response) => {
-    response.forEach((item, index) => {
-      if (item.flag !== '' && item.capital.length !== 0) {
-        const {
-          name: { common },
-          flag,
-          capital
-        } = item
+async function requestData() {
+  await fetch(url)
+    .then((res) => res.json())
+    .then((response) => {
+      response.forEach((item, index) => {
+        if (item.flag !== '' && item.capital.length !== 0) {
+          const {
+            name: { common },
+            flag,
+            capital
+          } = item
 
-        const temp = []
-        while (temp.length < 3) {
-          const randomIndex = Math.floor(Math.random() * (response.length - 1))
-          if (randomIndex !== index) {
-            temp.push(randomIndex)
+          const temp = []
+          while (temp.length < 3) {
+            const randomIndex = Math.floor(Math.random() * (response.length - 1))
+            if (randomIndex !== index) {
+              temp.push(randomIndex)
+            }
           }
+
+          requestedData.push({
+            question: `What is the capital of ${common}?`,
+            answer: [
+              { right: capital[0] },
+              { wrong: response[temp[0]].capital[0] },
+              { wrong: response[temp[1]].capital[0] },
+              { wrong: response[temp[2]].capital[0] }
+            ]
+          })
+
+          requestedData.push({
+            question: `Which country this flag ${flag} belongs to`,
+            answer: [
+              { right: common },
+              { wrong: response[temp[0]].name.common },
+              { wrong: response[temp[1]].name.common },
+              { wrong: response[temp[2]].name.common }
+            ]
+          })
         }
-
-        requestedData.push({
-          question: `What is the capital of ${common}?`,
-          answer: [
-            { right: capital[0] },
-            { wrong: response[temp[0]].capital[0] },
-            { wrong: response[temp[1]].capital[0] },
-            { wrong: response[temp[2]].capital[0] }
-          ]
-        })
-
-        requestedData.push({
-          question: `Which country this flag ${flag} belongs to`,
-          answer: [
-            { right: common },
-            { wrong: response[temp[0]].name.common },
-            { wrong: response[temp[1]].name.common },
-            { wrong: response[temp[2]].name.common }
-          ]
-        })
-      }
+      })
     })
-  })
-  .catch((error) => {
-    // eslint-disable-next-line no-alert
-    alert(`An error occurred while loading questions \n${error}`)
-  })
+    .catch((error) => {
+      // eslint-disable-next-line no-alert
+      alert(`An error occurred while loading questions \n${error}`)
+    })
+}
 
-export default requestedData
+requestData()
+
+export { requestedData }
