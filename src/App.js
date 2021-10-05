24b7567cb4f37ff5fb7d4452dd4330833a8b3420
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-05 20:37:24 +0200

    fix: add useEffect to re-render component after questions get loaded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useContext, useEffect } from 'react'
+
 import CardBox from './components/card-box/card-box.component'
+
+import { QuestionsContext } from './contexts/questions/questions.context'
 
 import './App.css'
 
 function App() {
+  const { getQuestions, nextQuestion, questions } = useContext(QuestionsContext)
+
+  useEffect(() => {
+    getQuestions()
+  }, [])
+
+  useEffect(() => {
+    nextQuestion()
+  }, [questions])
+
   return (
     <div className="App">
       <h1> Country Quiz </h1>
