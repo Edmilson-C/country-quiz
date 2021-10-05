e076dd10fbc2613d05754bbd0d589de697a6d63d
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-05 14:15:19 +0200

    feat: add button click handling

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable object-curly-newline */
 import React from 'react'
 
 import './button.styles.scss'
 
-const Button = ({ content, option, type }) => (
-  <button type="submit" className={`btn btn--${type}`}>
+const Button = ({ content, option, type, handleClick }) => (
+  <button type="submit" onClick={handleClick} className={`btn btn--${type}`}>
     {option && <span className="btn--answer__option">{option}</span>}
     {content}
   </button>
