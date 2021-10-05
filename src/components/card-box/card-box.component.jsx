16a88036b13afb6f9c3025672398de4da904dc24
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-06 00:45:51 +0200

    feat: get selected question & answer and randomly get wrong answers

--- src/components/card-box/card-box.component.jsx
+++ src/components/card-box/card-box.component.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Button from '../button/button.component'
 
+import { QuestionsContext } from '../../contexts/questions/questions.context'
+
 import './card-box.styles.scss'
 
-const CardBox = () => (
-  <div className="card-box">
-    <h3 className="card-box__heading"> Title will be here </h3>
-    <Button type="answer" option="A" content="Mozambique" />
-    <Button type="answer" option="B" content="Mozambique" />
-    <Button type="answer" option="C" content="Mozambique" />
-    <Button type="answer" option="D" content="Mozambique" />
-  </div>
-)
+const CardBox = () => {
+  const {
+    currentQuestion, questions, nextQuestion
+  } = useContext(QuestionsContext)
+
+  const temp = []
+
+  if (questions.length > 0 && currentQuestion) {
+    while (temp.length < 3) {
+      const randomIndex = Math.floor(Math.random() * (questions.length - 1))
+      temp.push(questions[randomIndex].answer)
+    }
+    temp.push(currentQuestion.answer)
+    temp.sort(() => 0.4 - Math.random())
+  }
+
+  return currentQuestion ? (
+    <div className="card-box">
+      <h3 className="card-box__heading">{currentQuestion.question}</h3>
+      <Button type="answer" option="A" content={temp[0]} />
+      <Button type="answer" option="B" content={temp[1]} />
+      <Button type="answer" option="C" content={temp[2]} />
+      <Button type="answer" option="D" content={temp[3]} />
+    </div>
+  ) : (
+    <div> Loading </div>
+  )
+}
 
 export default CardBox
