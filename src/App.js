e38ddc333ccc849146938aa6dad850fea8f7674d
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-10-06 13:13:06 +0200

    refactor: move answers loader to app.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,33 @@ import { QuestionsContext } from './contexts/questions/questions.context'
 import './App.css'
 
 function App() {
-  const { getQuestions, nextQuestion, questions } = useContext(QuestionsContext)
+  const {
+    getQuestions, nextQuestion, questions, currentQuestion
+  } = useContext(QuestionsContext)
 
   useEffect(() => {
     getQuestions()
-    nextQuestion()
   }, [])
 
   useEffect(() => {
     nextQuestion()
   }, [questions])
 
+  const answers = []
+
+  if (questions.length > 0 && currentQuestion) {
+    while (answers.length < 3) {
+      const randomIndex = Math.floor(Math.random() * (questions.length - 1))
+      answers.push(questions[randomIndex].answer)
+    }
+    answers.push(currentQuestion.answer)
+    answers.sort(() => 0.4 - Math.random())
+  }
+
   return (
     <div className="App">
       <h1> Country Quiz </h1>
-      <CardBox />
+      <CardBox answers={answers} />
     </div>
   )
 }
